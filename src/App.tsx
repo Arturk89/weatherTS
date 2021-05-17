c17dc4de0a44fa18b99cdbe5f6cf7b226a26f71e
Artur Kempny <artur.kempny@gmail.com>
2021-05-17 18:11:21 +0200

    Create basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import {Grid, TextField, Typography}  from '@material-ui/core';
+import { API_KEY, URL } from './data/contants';
 import './App.css';
 
+const useStyles = makeStyles((theme: Theme) => 
+  createStyles({
+    root: {
+      marginTop: '20px'
+    },
+    formContent: {
+      marginTop: '20px'
+    }
+  })
+)
+
+type Weather = {
+  feels_like: number;
+  humidity: number;
+  pressure: number;
+  temp: number;
+  temp_max: number;
+  temp_min: number;
+}
+
 function App() {
+  const classes = useStyles();
+
+  const [city, setCity] = useState<string>('');
+  const [details, setDetails] = useState<Weather | null>(null);
+  const ref = useRef<HTMLInputElement | null>(null);
+
+  const onChangeCity = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(event.target.value);
+  }
+
+  useEffect(() => {
+    ref.current?.focus();
+  }, [])
+  useEffect(() => {
+    const MAIN_URL: string = URL+city+API_KEY;
+    console.log(MAIN_URL)
+    fetch(MAIN_URL)
+      .then(response => response.json())
+      .then(data => setDetails(data.main))
+      .catch(err => console.error(err))
+  }, [city])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container spacing={1}>
+        <Grid className={classes.root} item xs={12}>
+          <Typography>Please type in your city</Typography>
+        </Grid>
+        <Grid className={classes.formContent} item xs={12}>
+          <TextField fullWidth label="City" inputRef={ref} value={city} onChange={onChangeCity} placeholder="Check your city"/>
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/data/contants.ts
@@ -0,0 +1,2 @@
+export const API_KEY: string = '&appid=747ed47cc094b264a83afa31173e764a';
+export const URL: string = 'https://api.openweathermap.org/data/2.5/weather?q=';
